e91fd789fdcba8e724a97b742cc160b7ec96489a
Piyush Mulik <piyushmulik14@gmail.com>
2017-01-31 12:21:13 +0530

    chaining of code | updated

--- gui.py
+++ gui.py
@@ -2,11 +2,16 @@ import tkFileDialog
 from Tkinter import *
 from main import *
 
+
 class openDialog(Frame):
-    def __init__(self, master=None):
+    dir_path = ""
+    mc = None
+
+    def __init__(self, mc, master=None):
         master.minsize(width=666, height=60)
         self.root = master
         self.createWidgets()
+        self.mc = mc
 
     def createWidgets(self):
         self.fFrame = Frame(self.root)
@@ -14,7 +19,7 @@ class openDialog(Frame):
         self.eDir = Entry(self.fFrame, width=70, textvariable=self.svDir)
         self.eDir.pack()
         self.bSelDir = Button(self.fFrame, text="select dir")
-        self.filterResume = Button(self.fFrame, text="filter resume" )
+        self.filterResume = Button(self.fFrame, text="filter resume")
         self.bSelDir["command"] = self.getDir
         self.filterResume["command"] = self.filter
         self.filterResume.pack(side=LEFT)
@@ -22,17 +27,15 @@ class openDialog(Frame):
         self.fFrame.pack()
 
     def getDir(self):
+        global dir_path
         self.dir = tkFileDialog.askdirectory()
         self.svDir.set(self.dir)
-        print self.dir
+        dir_path = self.dir
+        self.mc.set_current_dir(dir_path)
+
+    def getCurrentDir(self):
+        return dir_path
 
     def filter(self):
-        start()
+        self.mc.start()
         print "in filter function"
-
-
-if __name__ == "__main__":
-    root = Tk()
-    od = openDialog(root)
-    root.mainloop()
-
--- main.py
+++ main.py
@@ -1,69 +1,94 @@
 import PyPDF2
 import requests
 import glob
-from readTech import *
+from gui import *
+from reportlab.pdfgen import canvas
 
-technologies = ["Java", "c", "Oracle", "C++", "English", "HTML", "javascript"]
-#technologies = readList()
+technologies = ["Java","php", "c", "Oracle","jdbc","servlet", "C++", "English","hibernate", "HTML", "javascript", "Django","k"]
+list1 = [element.lower() for element in technologies]
 my_file = open("out.txt", "w")
 
-def readfile(filename):
-    file1 = open(filename, 'rb')
-    reader = PyPDF2.PdfFileReader(file1)
-    allText = ""
-    for i in xrange(reader.getNumPages()):
-        page = reader.getPage(i)
+class mainClass:
+    current_dir = ""
+    c = canvas.Canvas('test.pdf')
+    def set_current_dir(self, dir_name):
+        print dir_name
+        self.current_dir = dir_name
+
+    def readfile(self, filename):
+        file1 = open(filename, 'rb')
+        reader = PyPDF2.PdfFileReader(file1)
+        allText = ""
+        for i in xrange(reader.getNumPages()):
+            page = reader.getPage(i)
+            text = page.extractText()
+            allText += text
+        return allText
+
+    def getFullName(self, output):
+        for dataDict in output["entity_list"]:
+            sem = dataDict["sementity"]
+            type = sem['type']
+            if "FullName" in type:
+                print "Its a Full Name "
+                print dataDict["form"]
+                my_file.write("\n" + dataDict["form"] + " ----> ")
+                break
+
+    def init(self, fileText):
+        url = 'https://api.meaningcloud.com/topics-2.0'
+        params = {'key': '68e2c85289a717e6e2ff583c2548a529'}
+        data = {
+            'of': 'json',
+            'lang': 'en',
+            'tt': 'a',
+            'txt': fileText
+        }
+        r = requests.post(url, params=params, data=data)
+        output = r.json()
+        return output
+
+    def readPageOne(self, filename):
+        file = open(filename, 'rb')
+        reader = PyPDF2.PdfFileReader(file)
+        page = reader.getPage(0)
         text = page.extractText()
-        allText += text
-    return allText
+        return text
 
-def getFullName(output):
-    for dataDict in output["entity_list"]:
-        sem = dataDict["sementity"]
-        type = sem['type']
-        if "FullName" in type:
-            print "Its a Full Name "
-            print dataDict["form"]
-            my_file.write("\n"+dataDict["form"]+" ----> ")
-            break
+    def words_in_string(self, word_list, a_string):
+        word_list = [element.lower() for element in word_list]
+        return set(word_list).intersection(a_string.lower().split())
 
+    def start(self):
+        print self.current_dir
+        for filename in glob.glob(self.current_dir + '/*.pdf'):
+            print "files names :", filename
+            pageOneText = self.readPageOne(filename)
+            fileText = self.readfile(filename)
+            # print "filetext", fileText
+            splittedText = fileText.split(" ")
+            # list2 = [element.lower() for element in splittedText]
 
-def init(fileText):
-    url = 'https://api.meaningcloud.com/topics-2.0'
-    params = {'key': '68e2c85289a717e6e2ff583c2548a529'}
-    data = {
-        'of': 'json',
-        'lang': 'en',
-        'tt': 'a',
-        'txt': fileText
-    }
-    r = requests.post(url, params=params, data=data)
-    output = r.json()
-    return output
+            output = self.init(pageOneText)
+            self.getFullName(output)
 
-def readPageOne(filename):
-    file = open(filename, 'rb')
-    reader = PyPDF2.PdfFileReader(file)
-    page = reader.getPage(0)
-    text = page.extractText()
-    return text
+            for word in self.words_in_string(technologies, fileText):
+                my_file.write(" " + word + " ")
+                print(word)
 
-# if __name__ == "__main__":
-def start():
-    for filename in glob.glob('C:/Users/mulik_p/Desktop/resumes/*.pdf'):
+                # for tech in technologies:
+                #     var = str(tech)
+                #     if tech.lower() in fileText.lower():
+                #         print "found ", tech
+                #         my_file.write(" " + tech + " ")
+                #  if var.encode('utf-8').lower() in fileText.lower():
+                #  print var.encode('utf-8').lower()
+                # print "found ", tech
+                #  my_file.write(" " + tech + " ")
 
 
-        #my_file.write("Hello world")
-        print "files names :", filename
-        pageOneText = readPageOne(filename)
-        fileText = readfile(filename)
-        output = init(pageOneText)
-        getFullName(output)
-        for tech in technologies:
-            var = str(tech)
-            if var.encode('utf-8').lower() in fileText.lower():
-                print "found ",tech
-                my_file.write(" "+tech+" ")
-
-
-
+if __name__ == "__main__":
+    mc = mainClass()
+    root = Tk()
+    od = openDialog(mc, root)
+    root.mainloop()
